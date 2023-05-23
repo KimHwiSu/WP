958c438c1fd00d2dd72c4f1ebb976b24533920ea
ljk8654 <132908803+ljk8654@users.noreply.github.com>
2023-05-23 17:18:33 +0900

    ljk
    힘, 포탄궤적, 위아래키로 에임이동.

--- WindowProgramming/WindowProgramming/ball.h
+++ WindowProgramming/WindowProgramming/ball.h
@@ -14,6 +14,24 @@ public:
 		int currenty = y + sin(angle * PI / 180) * m * time + 0.5 * 10 * time * time; //현재 y좌표 g=10으로 가정
 		return currenty;
 	}
-	
+	//각도
+	float anlgepoint_x_up(int angle_x) {
+		angle_x;
+		return cos(angle_x * PI / 180);
+	}
+	float anlgepoint_y_up(int angle_y) {
+		angle_y;
+		return sin(angle_y * PI / 180);
+	}
+	float anlgepoint_x_down(int angle_x) {
+		angle_x;
+		return cos(angle_x * PI / 180);
+	}
+	float anlgepoint_y_down(int angle_y) {
+		angle_y;
+		return sin(angle_y * PI / 180);
+	}
+
 private:
+	int a = 0;
 };
--- WindowProgramming/WindowProgramming/main.cpp
+++ WindowProgramming/WindowProgramming/main.cpp
@@ -30,7 +30,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpszParam,
 	WndClass.hInstance = hInstance;
 	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	WndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
 	WndClass.lpszMenuName = NULL;
 	WndClass.lpszClassName = lpszClass;
 	WndClass.hIconSm = LoadIcon(NULL, IDI_QUESTION);
@@ -47,22 +47,35 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpszParam,
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
+	bullet bullet;
 	PAINTSTRUCT ps;
 	HDC hdc;
-
+	static int store_angle;
+	static int power;
+	static float bullet_x, bullet_y;
+	static int angle;
+	static int plus;
+	static int a;
+	static float aim_x,aim_y;
+	static int time;
+	static int act;
 	RECT clientRect;
 	GetClientRect(hWnd, &clientRect);
 
 	static Character obj;
 	switch (iMessage) {
 	case WM_CREATE:
+		a = 0;
+		plus = 0;
+		act = 0;
+		time = 0;
+		angle = 0;
+		bullet_x = 40;
+		bullet_y = 200;
+		aim_x = bullet.anlgepoint_x_up(angle);
+		aim_y = bullet.anlgepoint_y_up(angle);
 
 		// printf (로그) 뽑는 용
-		AllocConsole();
-		_tfreopen(_T("CONOUT$"), _T("w"), stdout);
-		_tfreopen(_T("CONIN$"), _T("r"), stdin);
-		_tfreopen(_T("CONERR$"), _T("w"), stderr);
-		SetTimer(hWnd, 1, 1000, NULL);
 		break;
 	case WM_LBUTTONDOWN:
 		// 원하는 지점 클릭하면 좌표 나옴
@@ -71,13 +84,115 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 		p.y = HIWORD(lParam);
 		cout << p.x << ", " << p.y << endl;
 		break;
-	case WM_TIMER:
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_UP:
+			if (act == 0) {
+				aim_x = bullet.anlgepoint_x_up(angle--);
+				aim_y = bullet.anlgepoint_y_up(angle);
 
-		InvalidateRect(hWnd, NULL, FALSE);
+			}
+			break;
+		case VK_DOWN:
+			if (act == 0) {
+				aim_x = bullet.anlgepoint_x_down(angle++);
+				aim_y = bullet.anlgepoint_y_down(angle);
+			}
+			break;
+			//위아래키로 에임 조정
+		case VK_SPACE:
+			if (act == 0) {
+				store_angle = angle;
+				act = 1;
+				SetTimer(hWnd, 1, 100, NULL);
+
+			}
+			
+			break;
+		default:
+			break;
+		}
+		break;
+	case WM_KEYUP:
+		switch (wParam)
+		{
+		case VK_UP:
+			
+			break;
+		case VK_DOWN:
+			
+			break;
+		case VK_SPACE:
+			
+				
+				if (act == 2) {
+					act = 3;
+					SetTimer(hWnd, 1, 100, NULL);
+				}
+				if (act == 1) {
+					act = 2;
+					SetTimer(hWnd, 1, 100, NULL);
+				}
+				plus = 0;
+			break;
+		default:
+			break;
+		}
+		break;
+	case WM_TIMER:
+		switch (wParam)
+		{
+		case 1:
+			if (act == 1) {
+				if (plus == 0) {
+					aim_x = bullet.anlgepoint_x_up(angle--);
+					aim_y = bullet.anlgepoint_y_up(angle);
+				}
+				 if(plus == 1) {
+					aim_x = bullet.anlgepoint_x_up(angle++);
+					aim_y = bullet.anlgepoint_y_up(angle);
+				}
+				if (angle < store_angle - 30 && plus == 0) {
+					plus = 1;
+				}
+				if (angle > store_angle + 30 && plus == 1) {
+					plus = 0;
+				}
+			}//에임 위아래 이동
+			if (act == 2) {
+				
+			if(plus == 0)power += 20;
+			if (plus == 1)power -= 20;
+			if (power == 300)plus = 1;
+			if (power == 0)plus = 0;
+			} //힘조절
+			if (act == 3) {
+				time++;
+				bullet_x = bullet.bullet_xmove(bullet_x, angle, power, time);
+				bullet_y = bullet.bullet_ymove(bullet_y, angle, power, time);
+				if (bullet_y > 600) {
+					time = 0;
+					bullet_x = 20;
+					bullet_y = 200;
+					act = 0;
+					plus = 0;
+				}
+			}//총알 궤적
+			break;
+		default:
+			break;
+		}
+		InvalidateRect(hWnd, NULL, TRUE);
 		break;
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
 		obj.draw(hdc);
+		MoveToEx(hdc, bullet_x+20, bullet_y+10, NULL);
+		LineTo(hdc, bullet_x + 20 * aim_x + 20, 20 * aim_y + bullet_y + 10);
+		MoveToEx(hdc, bullet_x -10, bullet_y + 20, NULL);
+		LineTo(hdc, bullet_x -10, -power + bullet_y );
+		Ellipse(hdc, bullet_x, bullet_y, bullet_x + 20, bullet_y + 20);
 		EndPaint(hWnd, &ps);
 		break;
 	case WM_DESTROY:
